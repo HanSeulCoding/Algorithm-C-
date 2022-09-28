d9b9a64ee0a28484d65c37c5964a549bff36d0bd
HanSeulCoding <lhslhss6843@gmail.com>
2022-09-28 19:39:00 +0900

    Update

--- Printer2(Programmers)/Printer2(Programmers)/Printer2.cpp
+++ Printer2(Programmers)/Printer2(Programmers)/Printer2.cpp
@@ -8,58 +8,57 @@ struct Compare
 {
     bool operator()(pair<int, int> a, pair<int, int> b)
     {
+        if (a.second == b.second)
+            return false;
         return a.second < b.second;
     }
 };
 
+bool Compare2(pair<int,int> a, pair<int,int> b)
+{
+    return a.second < b.second;
+}
 int solution(vector<int> priorities, int location) {
     int answer = 0;
     int maxIndex = 0;
     int max = -1;
-    priority_queue < pair<int, int>, vector<pair<int, int>>, Compare> pq;
+    int count = 0;
+    queue<pair<int, int>> q;
+    priority_queue <int> pq;
+
 
     for (int i = 0; i < priorities.size(); ++i)
     {
-        if (max < priorities[i])
-        {
-            max = priorities[i];
-            maxIndex = i;
-        }
+        q.push(make_pair(i, priorities[i]));
+        pq.push(priorities[i]);
     }
-    for (int i = maxIndex; i < priorities.size(); ++i)
-    {
-        pair<int, int> temp;
-        temp.first = i;
-        temp.second = priorities[i];
-        pq.push(temp);
-    }
-    for (int i = 0; i < maxIndex; ++i)
-    {
-        pair<int, int> temp;
-        temp.first = i;
-        temp.second = priorities[i];
-        pq.push(temp);
-    }
-    queue<pair<int, int>> tempQ;
-    while (!pq.empty())
-    {
-        pair<int, int> temp = pq.top();
-        pq.pop();
-        answer++;
-        if (temp.first == location)
-        {
-            break;
-        }
-    }
-    return answer + 1;
 
-    
+    while (!q.empty())
+    {
+        pair<int, int> temp = q.front();
+        int index = temp.first;
+        int value = temp.second;
+        q.pop();
+
+        if (pq.top() == value)
+        {
+            pq.pop();
+            answer++;
+            if (index == location)
+            {
+                break;
+            }
+        }
+        else
+            q.push(make_pair(index, value));
+    }
+    return answer;
 }
 
 int main()
 {
-    vector<int> priorities = { 1, 1, 9, 1, 1, 1 };//{ 2,1,3,2 };
-    int location = 0;//2;
+    vector<int> priorities = { 1, 1, 9, 1, 1, 1 };//{ 2,1,3,2 };;
+    int location = 0;
 
     int answer = solution(priorities, location);
     cout << answer << endl;
