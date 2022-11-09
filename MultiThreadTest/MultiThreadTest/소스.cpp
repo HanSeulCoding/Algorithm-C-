f03f01f04c5c1841175de40c9bbcf23f3316bdf8
HanSeulCoding <lhslhss6843@gmail.com>
2022-11-09 23:43:54 +0900

    Update

--- MultiThreadTest/MultiThreadTest/소스.cpp
+++ MultiThreadTest/MultiThreadTest/소스.cpp
@@ -30,24 +30,49 @@ public:
 
 int main()
 {
+	static condition_variable g_controller;
+	static std::mutex m;
 	auto Func([](string s) {
-		for (int i = 0; i < 5; ++i)
+		//for (int i = 0; i < 5; ++i)
+		//{
+		//	//
+		//	
+		//	//m.lock();
+		//	this_thread::sleep_for(chrono::milliseconds(1));
+		//	std::lock_guard<std::mutex> guard(m);
+
+		//	cout << this_thread::get_id() << "is Working" << endl;
+
+		//	cout << s << " : " << i << endl;
+		//	//m.unlock();
+		//}
+		while (true)
 		{
-			cout << this_thread::get_id() << "is Working" << endl;
-			cout << s << " : " << i << endl;
+			std::unique_lock<std::mutex> lock(m);
+			g_controller.wait(lock);
+			cout << "Hello" << s << endl;
 		}
 		});
 	
-	static std::mutex m;
+
 	thread t1 = thread(Func,"Hanseul");
-
-
-
-	t1.join();
 	thread t2 = thread(Func, "HanWool");
 
-	t2.join();
-	Func("YeongIn");
+
+	while (true)
+	{
+		string line;
+		cin >> line;
+		if (line == "q")
+		{
+			t1.detach();
+			t2.detach();
+			break;
+		}
+		unique_lock<std::mutex> lock(m);
+		g_controller.notify_all();
+	}
+	//Func("YeongIn");
 	//m.lock();
 
 	/*for (int i = 0; i < 100; ++i)
